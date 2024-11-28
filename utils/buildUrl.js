8769f0e64fbf9d2e824d00bdbde462a6c98e73c0
sameer-cnetric-space <sameer@cnetric.com>
2024-11-28 13:51:17 +0530

    Update buildUrl.js

--- utils/buildUrl.js
+++ utils/buildUrl.js
@@ -1,7 +1,8 @@
 const buildFileUrl = (req, filePath) => {
   if (!filePath) return null;
   // Get protocol (http or https) and host (e.g., localhost:3005 or domain)
-  const protocol = req.protocol;
+  // Use `X-Forwarded-Proto` header to determine the correct protocol, fallback to req.protocol
+  const protocol = req.get("X-Forwarded-Proto") || req.protocol;
   const host = req.get("host"); // Returns the domain name or IP with the port
 
   // Build the complete URL by concatenating the protocol, host, and file path
